a043654c0a4f2519351e74c6492632c979ee66ad
ginloy <liyongleow@gmail.com>
2024-06-25 09:05:59 +0800

    This works

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,25 +1,78 @@
-use std::os::unix::net::UnixListener;
+use std::{
+    io::{Read, Write},
+    os::unix::net::{UnixListener, UnixStream},
+    time::Duration,
+};
 
-use crate::get_socket_path;
-use anyhow::Result;
+use crate::{get_socket_path, Signal};
+use anyhow::{bail, Result};
+use zbus::{proxy, zvariant::OwnedFd};
 
-pub fn start() -> Result<()> {
+#[proxy(
+    default_service = "org.freedesktop.login1",
+    default_path = "/org/freedesktop/login1",
+    interface = "org.freedesktop.login1.Manager"
+)]
+trait Systemd {
+    fn inhibit(&self, what: &str, who: &str, why: &str, mode: &str) -> zbus::Result<OwnedFd>;
+}
+
+pub async fn start() -> Result<()> {
+    let system = zbus::Connection::system().await?;
+    let mut fd: Option<OwnedFd> = None;
+    let systemd_proxy = SystemdProxy::new(&system).await?;
     let socket_path = get_socket_path()?;
     if socket_path.try_exists()? {
         std::fs::remove_file(&socket_path)?;
+        println!("Removed old socket");
     }
 
     let socket = UnixListener::bind(&socket_path)?;
-    for connection in socket.incoming() {
-        match connection {
-            Err(e) => break,
-            Ok(stream) => {
-                std::thread::spawn(|| {
-                    println!("connected (server)");
-                });
-            }
-        }
+    for mut stream in socket.incoming().filter_map(|c| c.ok()) {
+        handle_connection(&mut stream, &systemd_proxy, &mut fd).await?;
     }
+    Ok(())
+}
 
+async fn handle_connection(
+    stream: &mut UnixStream,
+    proxy1: &SystemdProxy<'_>,
+    fd: &mut Option<OwnedFd>,
+) -> Result<()> {
+    let mut buf: Vec<u8> = vec![0; 64];
+    stream.set_read_timeout(Some(Duration::from_millis(200)))?;
+    let bytes = stream.read(&mut buf)?;
+    let request = rkyv::from_bytes::<Signal>(&buf[..bytes]);
+    match request {
+        Err(_) => bail!("Failed to deserialize request"),
+        Ok(s) => match s {
+            Signal::Query => {
+                if fd.is_some() {
+                    send(stream, Signal::On)
+                } else {
+                    send(stream, Signal::Off)
+                }
+            }
+            Signal::On => {
+                if fd.is_none() {
+                    let new_fd = proxy1
+                        .inhibit("idle", "inhibitor", "User request", "block")
+                        .await?;
+                    *fd = Some(new_fd);
+                }
+                send(stream, Signal::On)
+            }
+            Signal::Off => {
+                *fd = None;
+                send(stream, Signal::Off)
+            }
+        },
+    }
+}
+
+fn send(stream: &mut UnixStream, signal: Signal) -> Result<()> {
+    let msg = rkyv::to_bytes::<_, 64>(&signal)?;
+    stream.write_all(&msg)?;
+    stream.flush()?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use clap::{Parser, Subcommand};
+use std::{
+    io::{Read, Write},
+    os::unix::net::UnixStream,
+    path::PathBuf,
+    time::Duration,
+};
 
-#[derive(Parser)]
+use anyhow::{bail, Result};
+use clap::{Parser, Subcommand};
+use daemon::start;
+use fork::Fork;
+use rkyv::{Archive, Deserialize, Serialize};
+
+mod daemon;
+
+#[derive(Parser, Debug)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<CliSubcommand>,
+    command: Option<Signal>,
 }
 
-#[derive(Subcommand)]
-enum CliSubcommand {
+#[derive(Subcommand, Debug, Archive, Serialize, Deserialize, PartialEq, Eq)]
+#[archive(check_bytes, compare(PartialEq))]
+enum Signal {
     /// Query for inhibitor state
     Query,
     /// Activate idle inhibition
@@ -16,6 +30,65 @@ enum CliSubcommand {
     Off,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
+    let signal = args.command.unwrap_or(Signal::Query);
+    match get_stream() {
+        Err(_) => match fork::fork() {
+            Ok(Fork::Child) => {
+                // let _ = fork::close_fd();
+                smol::block_on(start())
+            }
+            Ok(Fork::Parent(_)) => {
+                // println!("Daemon started with pid {c}");
+                std::thread::sleep(Duration::from_millis(200));
+                let mut stream = get_stream()?;
+                handle_signal(&mut stream, signal)?;
+                Ok(())
+            }
+            Err(e) => {
+                bail!("Failed to start daemon: {e}");
+            }
+        },
+        Ok(mut stream) => {
+            handle_signal(&mut stream, signal)?;
+            Ok(())
+        }
+    }
+}
+
+fn handle_signal(stream: &mut UnixStream, signal: Signal) -> Result<()> {
+    let reply = send(stream, signal)?;
+    match reply {
+        Signal::On => println!("Idle inhibition ON"),
+        Signal::Off => println!("Idle inhibition OFF"),
+        _ => bail!("Unknown reply from daemon"),
+    }
+    Ok(())
+}
+
+fn get_socket_path() -> Result<PathBuf> {
+    let runtime_dir = PathBuf::from(std::env::var("XDG_RUNTIME_DIR")?);
+    Ok(runtime_dir.join("inhibitor.sock"))
+}
+
+fn get_stream() -> Result<UnixStream> {
+    Ok(UnixStream::connect(get_socket_path()?)?)
+}
+
+fn send(stream: &mut UnixStream, signal: Signal) -> Result<Signal> {
+    let msg = rkyv::to_bytes::<_, 64>(&signal)?;
+    stream.write_all(&msg)?;
+    stream.flush()?;
+    let mut buf: Vec<u8> = vec![0; 64];
+    stream.set_read_timeout(Some(Duration::from_millis(200)))?;
+    let bytes = stream.read(&mut buf)?;
+    if bytes == 0 {
+        bail!("No response from daemon");
+    }
+    let x = rkyv::from_bytes::<Signal>(&buf[..bytes]);
+    match x {
+        Ok(x) => Ok(x),
+        Err(_) => bail!("Failed to deserialize response"),
+    }
 }
